957660caa9df290ac9401a2e3b6ebcbf7c5a2ef5
Neel <neel.choudhary3@gmail.com>
2019-01-21 15:58:27 -0500

    Solved Container

---
+++ src/Container.java
@@ -0,0 +1,34 @@
+
+public class Container {
+
+  public static void main(String[] args) {
+    SolutionContainer s = new SolutionContainer();
+    System.out.println(s.maxArea(new int[] {2,3,4,5,18,17,6}));
+  }
+
+}
+
+class SolutionContainer {
+
+  // Solution using two pointers
+  // Time Complexity T(n)
+  
+  public int maxArea(int[] height) {
+
+    int left = 0;
+    int right = height.length-1;
+    int largestArea = 0;
+
+    for (int i = 0; i < height.length; i++) {
+      int currArea = Math.min(height[left], height[right]) * (right - left);
+      largestArea = Math.max(currArea, largestArea);
+      if (height[right] < height[left]) {
+        right--;
+      } else {
+        left++;
+      }
+    }
+    return largestArea;
+  }
+
+}
